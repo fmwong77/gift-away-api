28aab038ab19a05eea0d4a7d072d0b7f8a4a16c0
fmwong77 <fongmeiw@gmail.com>
2020-02-10 20:42:31 -0600

    bugs fixing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
     def index
-        redirect_to :file => 'public/index.html'
+        # redirect_to :file => 'public/index.html'
+        redirect_to 'http://localhost:3001/home'
     end
 end
